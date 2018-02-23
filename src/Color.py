b6e7e81d95391294d01c96cf77bc2978cd0e1c18
Simon Trieb <triebsim>
2018-02-23 10:40:51 +0100

    Added color change detection

--- src/Color.py
+++ src/Color.py
@@ -1,23 +1,26 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.ma as ma
 
 cap = cv2.VideoCapture(1)
 
 prev = None
+ptmp = None
 
 means = np.zeros(100)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 Ln, = ax.plot(means, 'o-')
-ax.set_ylim([-1, 1])
+ax.set_ylim([-0.05, 0.05])
 plt.ion()
 plt.show()
 
+i = 0
 
 while(1):
-    key = cv2.waitKey(20) & 0xFF
+    key = cv2.waitKey(1) & 0xFF
 
     ret, frame = cap.read()
     if not ret:
@@ -42,20 +45,38 @@ while(1):
     img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR) 
     img = np.uint8(img * 255)
     cv2.imshow('test', img)
-    
     if prev is not None: 
-        diff = (np.mod(hsv[:,:,0]-prev[:,:,0] + 180, 360) - 180) * (hsv[:,:,1] + prev[:,:,1]) * (hsv[:,:,2] + prev[:,:,2]) / 4
-        #diff = np.clip(diff-10,0,360)
-        means = np.append(means, [np.mean(diff)])
-        means = means[-100:]
+        level = hsv[:,:,1] * prev[:,:,1] * hsv[:,:,2] * prev[:,:,2]
+        levelMask = ma.masked_less(level,0.5) 
+        diff =  (np.mod(hsv[:,:,0]-prev[:,:,0] + 180, 360) - 180)
+        negative = ma.masked_outside(diff,-120,-90) * levelMask
+        positive = ma.masked_outside(diff,90, 120) * levelMask
+       #diff = np.clip(diff-10,0,360)
+       # means = np.append(means, [np.mean(diff)])
+        means = np.append([(negative.count() - positive.count()) / diff.shape[0] / diff.shape[1]],means)
+        means = means[:100]
+
+        fps = 20
+        if(means[1] < - 0.002 and means[0] > means[1]):
+            last2secs = means[:fps]
+            minLoc = last2secs.argmin()
+            maxLoc = last2secs.argmax()
+            min = last2secs[minLoc]
+            max = last2secs[maxLoc]
+            if minLoc == 1 and last2secs[3:].min() > min * 0.5 and max > -min * 0.5 and maxLoc > fps*0.4:
+                print(i)
+                
 
         Ln.set_ydata(means)
         Ln.set_xdata(range(len(means)))
 
-        cv2.imshow('diff', diff / 360 + 0.5)
 
-    prev = hsv
+        cv2.imshow('levelmask', levelMask )
+        cv2.imshow('diff', negative.filled(0) / 360 + 0.5)
 
+    prev = ptmp
+    ptmp = hsv
+    i += 1
 
     #frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
     cv2.imshow('frame', frame)
